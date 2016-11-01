df152175347a308d3adef75195aecc4a5c0d636c
airtoxin <airtoxin@icloud.com>
2016-11-02 08:08:51 +0900

    Add header link

--- src/views/Header/component.js
+++ src/views/Header/component.js
@@ -3,8 +3,20 @@ import styles from './component.css';
 
 export default (props) => {
   return (
-    <div className={`${styles.noSpace} ${styles.black}`}>
-      <h1>Yavalath</h1>
+    <div className={`${styles.noSpace} ${styles.black} ${styles.flex}`}>
+      <h1 className={styles.flexLeft}>
+        Yavalath
+      </h1>
+      <h1 className={styles.flexRight}>
+        <a href="http://www.cameronius.com/games/yavalath/" target="_blank" className={styles.link}>
+          Rule <i className="fa fa-link" aria-hidden="true"></i>
+        </a>
+      </h1>
+      <h1 className={styles.flexRight}>
+        <a href="https://github.com/airtoxin/yavalath-baobab" target="_blank" className={styles.link}>
+          <i className="fa fa-github" aria-hidden="true"></i>
+        </a>
+      </h1>
     </div>
   );
 };
