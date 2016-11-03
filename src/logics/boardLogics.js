dbd1be51debc63a828769539fa74068b9f4853aa
airtoxin <airtoxin@icloud.com>
2016-11-03 19:33:30 +0900

    Fix win-lose check logic

--- src/logics/boardLogics.js
+++ src/logics/boardLogics.js
@@ -2,70 +2,127 @@
 import { flatten } from 'lodash';
 import { constants } from '../tree';
 
+const checkLose = (sortedGrids, { gridX, gridY, occupiedPlayer }) => {
+  /* pattern 1 (3 o'clock direction) */
+  try {
+    const isLose = (
+      sortedGrids[gridY][gridX + 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY][gridX + 2].occupiedPlayer.id === occupiedPlayer.id);
+    if (isLose) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 2 (9 o'clock direction) */
+  try {
+    const isLose = (
+      sortedGrids[gridY][gridX - 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY][gridX - 2].occupiedPlayer.id === occupiedPlayer.id);
+    if (isLose) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 3 (5 o'clock direction) */
+  try {
+    const isLose = (
+      sortedGrids[gridY + 1][gridX].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY + 2][gridX].occupiedPlayer.id === occupiedPlayer.id);
+    if (isLose) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 4 (11 o'clock direction) */
+  try {
+    const isLose = (
+      sortedGrids[gridY - 1][gridX].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY - 2][gridX].occupiedPlayer.id === occupiedPlayer.id);
+    if (isLose) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 5 (1 o'clock direction) */
+  try {
+    const isLose = (
+      sortedGrids[gridY - 1][gridX + 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY - 2][gridX + 2].occupiedPlayer.id === occupiedPlayer.id);
+    if (isLose) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 6 (7 o'clock direction) */
+  try {
+    const isLose = (
+      sortedGrids[gridY + 1][gridX - 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY + 2][gridX - 2].occupiedPlayer.id === occupiedPlayer.id);
+    if (isLose) return true;
+  } catch (e) { /* pass */ }
+
+  return false;
+};
+
+const checkWin = (sortedGrids, { gridX, gridY, occupiedPlayer }) => {
+  /* pattern 1 (3 o'clock direction) */
+  try {
+    const isWin = (
+      sortedGrids[gridY][gridX + 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY][gridX + 2].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY][gridX + 3].occupiedPlayer.id === occupiedPlayer.id);
+    if (isWin) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 2 (9 o'clock direction) */
+  try {
+    const isWin = (
+      sortedGrids[gridY][gridX - 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY][gridX - 2].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY][gridX - 3].occupiedPlayer.id === occupiedPlayer.id);
+    if (isWin) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 3 (5 o'clock direction) */
+  try {
+    const isWin = (
+      sortedGrids[gridY + 1][gridX].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY + 2][gridX].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY + 3][gridX].occupiedPlayer.id === occupiedPlayer.id);
+    if (isWin) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 4 (11 o'clock direction) */
+  try {
+    const isWin = (
+      sortedGrids[gridY - 1][gridX].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY - 2][gridX].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY - 3][gridX].occupiedPlayer.id === occupiedPlayer.id);
+    if (isWin) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 5 (1 o'clock direction) */
+  try {
+    const isWin = (
+      sortedGrids[gridY - 1][gridX + 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY - 2][gridX + 2].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY - 3][gridX + 3].occupiedPlayer.id === occupiedPlayer.id);
+    if (isWin) return true;
+  } catch (e) { /* pass */ }
+
+  /* pattern 6 (7 o'clock direction) */
+  try {
+    const isWin = (
+      sortedGrids[gridY + 1][gridX - 1].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY + 2][gridX - 2].occupiedPlayer.id === occupiedPlayer.id &&
+      sortedGrids[gridY + 3][gridX - 3].occupiedPlayer.id === occupiedPlayer.id);
+    if (isWin) return true;
+  } catch (e) { /* pass */ }
+
+  return false;
+};
+
 export const checkFinish = (sortedGrids) => {
   const filledGrids = flatten(sortedGrids).filter(({ state, occupiedPlayer }) => state === constants.gridStates.occupied && occupiedPlayer !== null);
 
-  for (const { gridX, gridY, occupiedPlayer } of filledGrids) {
-    /* pattern 1 (3 o'clock direction) */
-    try {
-      const isLose = sortedGrids[gridY][gridX + 1].occupiedPlayer.id === occupiedPlayer.id && sortedGrids[gridY][gridX + 2].occupiedPlayer.id === occupiedPlayer.id;
-      try {
-        const isWin = isLose && sortedGrids[gridY][gridX + 3].occupiedPlayer.id === occupiedPlayer.id;
-        if (isWin) return true;
-      } catch (e) { /* pass */ }
-      if (isLose) return false;
-    } catch (e) { /* pass */ }
+  let losing = null;
 
-    /* pattern 2 (9 o'clock direction) */
-    try {
-      const isLose = sortedGrids[gridY][gridX - 1].occupiedPlayer.id === occupiedPlayer.id && sortedGrids[gridY][gridX - 2].occupiedPlayer.id === occupiedPlayer.id;
-      try {
-        const isWin = isLose && sortedGrids[gridY][gridX - 3].occupiedPlayer.id === occupiedPlayer.id;
-        if (isWin) return true;
-      } catch (e) { /* pass */ }
-      if (isLose) return false;
-    } catch (e) { /* pass */ }
-
-    /* pattern 3 (5 o'clock direction) */
-    try {
-      const isLose = sortedGrids[gridY + 1][gridX].occupiedPlayer.id === occupiedPlayer.id && sortedGrids[gridY + 2][gridX].occupiedPlayer.id === occupiedPlayer.id;
-      try {
-        const isWin = isLose && sortedGrids[gridY + 3][gridX].occupiedPlayer.id === occupiedPlayer.id;
-        if (isWin) return true;
-      } catch (e) { /* pass */ }
-      if (isLose) return false;
-    } catch (e) { /* pass */ }
-
-    /* pattern 4 (11 o'clock direction) */
-    try {
-      const isLose = sortedGrids[gridY - 1][gridX].occupiedPlayer.id === occupiedPlayer.id && sortedGrids[gridY - 2][gridX].occupiedPlayer.id === occupiedPlayer.id;
-      try {
-        const isWin = isLose && sortedGrids[gridY - 3][gridX].occupiedPlayer.id === occupiedPlayer.id;
-        if (isWin) return true;
-      } catch (e) { /* pass */ }
-      if (isLose) return false;
-    } catch (e) { /* pass */ }
-
-    /* pattern 5 (1 o'clock direction) */
-    try {
-      const isLose = sortedGrids[gridY - 1][gridX + 1].occupiedPlayer.id === occupiedPlayer.id && sortedGrids[gridY - 2][gridX + 2].occupiedPlayer.id === occupiedPlayer.id;
-      try {
-        const isWin = isLose && sortedGrids[gridY - 3][gridX + 3].occupiedPlayer.id === occupiedPlayer.id;
-        if (isWin) return true;
-      } catch (e) { /* pass */ }
-      if (isLose) return false;
-    } catch (e) { /* pass */ }
-
-    /* pattern 6 (1 o'clock direction) */
-    try {
-      const isLose = sortedGrids[gridY + 1][gridX - 1].occupiedPlayer.id === occupiedPlayer.id && sortedGrids[gridY + 2][gridX - 2].occupiedPlayer.id === occupiedPlayer.id;
-      try {
-        const isWin = isLose && sortedGrids[gridY + 3][gridX - 3].occupiedPlayer.id === occupiedPlayer.id;
-        if (isWin) return true;
-      } catch (e) { /* pass */ }
-      if (isLose) return false;
-    } catch (e) { /* pass */ }
+  for (const filledGrid of filledGrids) {
+    if (checkWin(sortedGrids, filledGrid)) return true;
+    if (checkLose(sortedGrids, filledGrid)) losing = true;
   }
+
+  if (losing) return false;
 
   return null;
 };
